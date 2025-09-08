085912bdbd257e87312c5196f84dba76632842bb
FelixBruckmeier <felixbruckmeier@googlemail.com>
2025-09-08 18:01:34 +0200

    Add files via upload

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,11 @@
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./index.css";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App'
+import './index.css'
 
-createRoot(document.getElementById("root")!).render(<App />);
+const root = document.getElementById('root')!
+createRoot(root).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+)
