ba9b8754386c74c690213ee72bfbd00f8e93b3f8
FelixBruckmeier <felixbruckmeier@googlemail.com>
2025-09-09 09:10:03 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,10 @@
-import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { StrictMode } from "react";
-import { BrowserRouter } from "react-router-dom";
-import App from "@/App";
-import "@/i18n";
+import "./index.css";
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
